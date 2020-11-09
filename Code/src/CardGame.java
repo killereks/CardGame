166b3649361c9987c5c2e8ca7f869361b1df807b
BarnabasG <BarnabasG@users.noreply.github.com>
2020-11-09 13:15:01 +0000

    Update CardGame.java

--- Code/src/CardGame.java
+++ Code/src/CardGame.java
@@ -11,12 +11,41 @@ public class CardGame {
 	ArrayList<Player> players = new ArrayList<Player>();
 	ArrayList<CardDeck> cards = new ArrayList<CardDeck>();
 	
-	public void initGame(){
+	/**
+	 * Initialises the players hands and the cards in each deck
+	 * from the cards in the pack
+	 *
+	 * @param The ArrayList of cards from the file
+	 * @author 690022392
+	 */	
+	public void initGame(ArrayList<Integer> cards){
+
+		ArrayList<Integer> hand = new ArrayList<Integer>();
+		ArrayList<ArrayList<Integer>> playerHands = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> cardDecks = new ArrayList<ArrayList<Integer>>();
+		//int[] hand = new int();
+		//ArrayList<int[]> playerHands = new ArrayList<int[]>();
+		//ArrayList<int[]> cardDecks = new ArrayList<int[]>();
 		/**
 		 * assign cards to each player and deck
 		 * check determine winner
 		 * 
 		 */
+		System.out.println(cards);
+		for (int i = 1; i < cards.size() + 1; i++) {		
+			hand.add(cards.get(i-1));
+			if ((i % 4 == 0)) {
+				if (i <= (cards.size()/2)) {
+					playerHands.add(new ArrayList<Integer>(hand));
+					//Player player = new Player(new ArrayList<Integer>(hand))); DOESNT WORK
+				} else {
+					cardDecks.add(new ArrayList<Integer>(hand));
+				}
+				hand.clear();
+			}
+		}
+		System.out.println(playerHands);
+		System.out.println(cardDecks);
 	}
 	
 	public void GameStep(){
@@ -54,7 +83,10 @@ public class CardGame {
 		String name = reader.readLine();
 		System.out.println("Hello! "+name);*/
 		
-		System.out.println(CardsFromFile("cards.txt"));
+		ArrayList<Integer> cards = CardsFromFile("four.txt");
+		//System.out.println(cards);
+		CardGame game = new CardGame();
+		game.initGame(cards);
 	}
 
 	/**
